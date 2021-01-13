9cf83533d8d35603a466493ca8c1599e92750a70
Ernest Williams <ernest.williams@gatech.edu>
2021-01-13 17:33:24 -0600

    prevent use of sign in page when signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, redirect, Redirect } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -47,7 +47,10 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUp} />
+          <Route exact path='/signin'
+            render={() => this.props.currentUser ? (
+              <Redirect to='/' />) : (<SignInAndSignUp />)
+            } />
         </Switch>
       </div>
     );
@@ -57,4 +60,8 @@ class App extends React.Component {
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null, mapDispatchToProps)(App);
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -30,4 +30,5 @@ const Header = ({ currentUser }) => (
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser
 })
+
 export default connect(mapStateToProps)(Header);
