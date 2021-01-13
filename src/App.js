52a40d84658407b199d4786d67e825a00bde8d67
Ernest Williams <ernest.williams@gatech.edu>
2021-01-13 11:52:56 -0600

    added sign in component and custom button component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
