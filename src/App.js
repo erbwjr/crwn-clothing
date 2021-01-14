f8f6ddb0ac724b389802574b46fdbda6bb0c0b78
Ernest Williams <ernest.williams@gatech.edu>
2021-01-14 16:52:46 -0600

    added selectors for cart to prevent unecessary rerendering using memoization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, redirect, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -30,7 +30,7 @@ class App extends React.Component {
         });
 
       } else {
-        setCurrentUser({ userAuth })
+        setCurrentUser(userAuth)
       }
     })
   }
@@ -57,11 +57,12 @@ class App extends React.Component {
   }
 
 }
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
 
 const mapStateToProps = ({ user }) => ({
   currentUser: user.currentUser
-})
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 
 import './cart-icon.styles.scss';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+//selector: get full state, pull off a piece and use it
+//not carefully using mapStateToProps can cause unecessary rendering of components because reducers always return a new object which causes a rerender
+//memoization addresses this issue
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
